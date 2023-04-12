c0aaf5ace421743f9c3c92e5c68e585e1cfd8904
mdmmr97 <mdmmr97@hotmail.com>
2023-04-12 09:37:35 +0200

    implementamos Singleton en Logs

--- src/main/java/com/kreitek/pets/Logs.java
+++ src/main/java/com/kreitek/pets/Logs.java
@@ -1,9 +1,27 @@
 package com.kreitek.pets;
 
+import com.kreitek.pets.infraestructure.bd.DbService;
+
 public class Logs {
+    private static Logs instance = null;
+    private Logs() {
+        if (instance != null) {
+            throw new RuntimeException("Usage getInstance() method to create");
+        }
+    }
+    public static Logs getInstance() {
+        if (instance == null) {
+            synchronized(Logs.class) {
+                if (instance == null) {
+                    instance = new Logs();
+                }
+            }
+        }
+        return instance;
+    }
     private int contador = 0;
     public void Debug (String mensaje){
         contador++;
-        System.out.printf("[DEBUG][%d] - %s", contador, mensaje);
+        System.out.printf("[DEBUG][%d] - %s \n", contador, mensaje);
     }
 }
